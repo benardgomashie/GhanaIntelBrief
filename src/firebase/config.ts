ba909c7088f41f3e367aa319695a8bc5a3b8f3c6
Benard Gomashie <benardgomashie@gmail.com>
2026-02-17 09:37:30 +0000

    GitHub Action needed: Secrets detected in benardgomashie/GhanaIntelBrief

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,7 @@
 export const firebaseConfig = {
-  "projectId": "studio-5727160516-10668",
-  "appId": "1:506804102148:web:9a072957d578e50c0a0151",
-  "apiKey": "AIzaSyCER3IWpZWoCbZ1OW3h_S7J9-iqH_ef4Ws",
-  "authDomain": "studio-5727160516-10668.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "506804102148"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
